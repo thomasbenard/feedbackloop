8e8145ec963498a3d4b04ba48d69dfe4b68b3f01
BENARD Thomas <m.thomas.benard@gmail.com>
2017-10-08 22:36:05 +0200

    Update main

--- src/main/java/com/thomasbenard/feedbackloop/Main.java
+++ src/main/java/com/thomasbenard/feedbackloop/Main.java
@@ -4,19 +4,23 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        int moveDistance = 1;
-        System.out.println("Result for moveDistance " + moveDistance + " is " +
-                Scenario.computeAverageReachingDuration(runScenarios(moveDistance), moveDistance));
-        moveDistance = 100;
-        System.out.println("Result for moveDistance " + moveDistance + " is " +
-                Scenario.computeAverageReachingDuration(runScenarios(moveDistance), moveDistance));
+        runSimulation(1);
+        runSimulation(10);
+        runSimulation(100);
+        runSimulation(1000);
+    }
 
+    private static void runSimulation(int moveDistance) {
+        System.out.println("Result for moveDistance " + moveDistance + " is " +
+                Scenario.computeAverageReachingDuration(runScenarios(moveDistance), moveDistance));
     }
 
     private static ArrayList<Integer> runScenarios(int moveDistance) {
         ArrayList<Integer> scenarioResults = new ArrayList<Integer>();
         for (int i = 0; i < 10000; i++) {
-            Scenario scenario = new Scenario(new Position(0, 0), new Position(1000, 1500), new RandomDirectionGenerator(2));
+            Scenario scenario = new Scenario(new Position(0, 0, 0, 0, 0),
+                    new Position(1000, 1000, 1000, 1000, 1000),
+                    new RandomDirectionGenerator(5));
             scenarioResults.add(scenario.run(moveDistance));
         }
         return scenarioResults;
